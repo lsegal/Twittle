cb6edaff93b2eb293044dff3c2ff8c50ea009907
Loren Segal <lsegal@soen.ca>
2008-10-15 06:58:42 +0000

    Add parametrized callback template

--- twittle/include/callback.h
+++ twittle/include/callback.h
@@ -1,3 +1,5 @@
+#pragma once
+
 template <typename T>
 class Callback
 {
@@ -7,4 +9,15 @@ public:
 private:
 	T& obj;
 	void (T::*func)();
+};
+
+template <typename T, typename P1>
+class Callback1
+{
+public:
+	Callback1(T& obj, void (T::*func)(P1&)) : obj(obj), func(func) { }
+	void Call(P1& p1) { (obj.*func)(p1); }
+private:
+	T& obj;
+	void (T::*func)(P1& p1);
 };
