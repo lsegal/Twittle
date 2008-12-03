7174892e831a2c262d54ff6f45527ccf4a038891
Loren Segal <lsegal@soen.ca>
2008-12-03 22:32:52 +0000

    Refactor crlf code

--- twittle/src/multipart.cpp
+++ twittle/src/multipart.cpp
@@ -1,9 +1,9 @@
 #include "multipart.h"
-#include <wx/sstream.h>
+
+static wxString crlf = _T("\n");
 
 void Multipart::Add(const wxString& name, const wxString& value)
 {
-	wxString crlf = _T("\r\n");
 	buffer << _T("--") << boundary << crlf;
 	buffer << _T("Content-Disposition: form-data; name=\"") << name << _T("\"") << crlf << crlf;
 	buffer << value << crlf;
@@ -12,18 +12,15 @@ void Multipart::Add(const wxString& name, const wxString& value)
 void Multipart::AddFile(const wxString& name, const wxString& value, 
 						const wxString& filename, const wxString& mimeType)
 {
-	wxString crlf = _T("\r\n");
 	buffer << _T("--") << boundary << crlf;
 	buffer << _T("Content-Disposition: form-data; name=\"") << name << _T("\"; ");
 	buffer << _T("filename=\"") << filename << _T("\"") << crlf;
 	buffer << _T("Content-Type: ") << mimeType << crlf;
 	buffer << _T("Content-Transfer-Encoding: binary") << crlf << crlf;
-	int len = value.length();
-	buffer.Append(value);
-	buffer << crlf;
+	buffer << value << crlf;
 }
 
 void Multipart::End()
 {
-	buffer << _T("--") << boundary << _T("--\r\n");
+	buffer << _T("--") << boundary << _T("--") << crlf;
 }
