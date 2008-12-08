de24332fe2088ca022fa66f45f48b92c21df1db4
Loren Segal <lsegal@soen.ca>
2008-12-08 01:13:24 +0000

    Document about_dialog.h

--- twittle/include/about_dialog.h
+++ twittle/include/about_dialog.h
@@ -2,10 +2,20 @@
 
 #include <wx/wx.h>
 
+/**
+ * Represents a modal About dialog in the application showing 
+ * version and author information.
+ */
 class AboutDialog : public wxDialog
 {
+	/** Initializes all of the dialog controls */
 	void InitializeComponents();
 
 public:
+	/**
+	 * Creates a new About dialog with the parent window set.
+	 *
+	 * @param parent the parent window of the modal dialog (most likely MainWindow)
+	 */
 	AboutDialog(wxWindow *parent);
 };
