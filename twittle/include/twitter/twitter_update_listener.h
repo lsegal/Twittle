db8043eb59c3e67971a195b4ee723c6e16223ad3
Loren Segal <lsegal@soen.ca>
2008-12-08 09:02:32 +0000

    Doc TwitterUpdateListener

--- twittle/include/twitter/twitter_update_listener.h
+++ twittle/include/twitter/twitter_update_listener.h
@@ -5,9 +5,23 @@
 
 class Twitter;
 
+/** 
+ * An interface for the observer pattern used by Twitter to notify listeners of Twitter updates.
+ * This is mainly used by FeedPanel to receive its updates
+ */
 class TwitterUpdateListener
 {
 public:
+	/** Allow overriding */
 	virtual ~TwitterUpdateListener() { }
+
+	/** 
+	 * Implement this method to handle an update notification from Twitter on the string 
+	 * resource identifier.
+	 *
+	 * @param twitter the twitter object which initiated the notification
+	 * @param resource the resource name representing the feed that was updated. Use Twitter::GetFeed
+	 *   to retrieve the feed associated with the resource.
+	 */
 	virtual void TwitterUpdateReceived(const Twitter& twitter, const wxString& resource) = 0;
 };
