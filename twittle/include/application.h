cd552c91958910b8dfc26a7152425eda673ac550
Loren Segal <lsegal@soen.ca>
2008-12-08 01:13:15 +0000

    Document application.h

--- twittle/include/application.h
+++ twittle/include/application.h
@@ -6,6 +6,9 @@
 
 class Twitter;
 
+/**
+ * The main application class. 
+ */
 class Application : public wxApp
 {
 	MainWindow *main;
@@ -13,24 +16,76 @@ class Application : public wxApp
 	Twitter *twitter;
 
 public:
+	/** The application name */
 	static const wxString APPNAME;
+
+	/** The application version */
 	static const wxString APPVERSION;
 
-	// Opens a URL in the default browser
+	/**
+	 * Opens a URL in the default browser. Uses default browser
+	 * settings (Firefox can be set to open in a new tab)
+	 *
+	 * @param url the URL to open in the browser 
+	 */
 	static void OpenUrl(const wxString& url);
 
+	/** @return the main window frame */
 	inline MainWindow& GetMainWindow() { return *main; }
+
+	/** @return the Twitter client controlling the connection */
 	inline Twitter& GetTwitter() { return *twitter; }
+
+	/** @return the application settings */
 	inline Settings& GetSettings() { return settings; }
 
+	/** 
+	 * Initialization code.
+	 *
+	 * @override wxApp
+	 */
 	bool OnInit();
+
+	/** 
+	 * Called when the application exits.
+	 * Cleans up twitter objects and deserializes all
+	 * settings and data.
+	 *
+	 * @override wxApp
+	 */
 	int OnExit();
 
+	/**
+	 * Sets the auth info for the twitter account and shows the main
+	 * panel. Does not do any verification that the user/password
+	 * is valid; use Twitter::VerifyCredentials
+	 *
+	 * @see Twitter::VerifyCredentials
+	 * @param user the username 
+	 * @param pass the Twitter account password
+	 */
 	void Login(const wxString& user, const wxString& pass);
+
+	/** 
+	 * Logs out of the Twitter account by showing the login panel.
+	 * Does not actually talk to the Twitter server to formally "log out".
+	 */
 	void Logout();
 
+	/**
+	 * @return the path on disk to store all feed related data for the logged in
+	 *   user.
+	 */
 	const wxString GetFeedsPath();
+
+	/**
+	 * @return the path on disk to store all image related data for profile images
+	 */
 	const wxString GetImagesPath();
+
+	/**
+	 * @return the path on disk where the settings.xml file is stored
+	 */
 	const wxString GetSettingsPath();
 };
 
