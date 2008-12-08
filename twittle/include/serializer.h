8af6117583d370d22e76f359cc89448390a9ffb9
Loren Segal <lsegal@soen.ca>
2008-12-08 07:02:51 +0000

    Doc for serializer

--- twittle/include/serializer.h
+++ twittle/include/serializer.h
@@ -1,5 +1,14 @@
 #pragma once
 
+/**
+ * Basic interface to perform serialization on an object to any output.
+ * Implement this class by specializing the template for the types
+ * S and T where S represents the path representation and T represents 
+ * the class to be serialized. For file based serialization, S can be
+ * the filesystem path represented as a std::string or wxString
+ *
+ * @see Serializer<wxString,TwitterFeed>
+ */
 template<typename S, typename T>
 class Serializer
 {
@@ -7,6 +16,19 @@ private:
 	Serializer() { }
 	~Serializer() { }
 public:
+	/**
+	 * Performs deserialization from a resource locator to an object.
+	 *
+	 * @param path the location of the serialized object to read
+	 * @return the deserialized object
+	 */
 	static T *Read(const S& path);
-	static void Write(const S& path, const T& feed);
+
+	/**
+	 * Performs serialization on an object to resource locator.
+	 *
+	 * @param path the location of the serialized object to write to
+	 * @param obj the object to serialize
+	 */
+	static void Write(const S& path, const T& obj);
 };
